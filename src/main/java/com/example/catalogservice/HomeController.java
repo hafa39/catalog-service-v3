2eb9484f96bc402277ea6d09c5fe921604f5d2ae
Ruslan Khafizov <stu217082@mail.uni-kiel.de>
2023-10-04 18:01:25 +0200

    configs

--- src/main/java/com/example/catalogservice/HomeController.java
+++ src/main/java/com/example/catalogservice/HomeController.java
@@ -1,13 +1,20 @@
 package com.example.catalogservice;
 
+import com.example.catalogservice.config.PolarProperties;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
 
+    private PolarProperties properties;
+
+    public HomeController(PolarProperties properties) {
+        this.properties = properties;
+    }
+
     @GetMapping("/")
     public String greeting(){
-        return "Welcome to the book catalog!";
+        return properties.getGreeting();
     }
 }
